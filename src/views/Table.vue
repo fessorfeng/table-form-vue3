43983e5ecaea48bf823bebe97e81f4d46f3e9f44
fessor <352475718@qq.com>
2022-01-03 09:28:49 +0800

    删除多余文件留下虚拟滚动（表格与非表格）

--- src/views/Table.vue
+++ src/views/Table.vue
@@ -1,138 +1,157 @@
 <template>
-  <el-scrollbar
-    wrap-class="scrollbar__wrap"
-    style="max-width: 360px; background: yellow;position:relative;"
-    :viewStyle="`height: ${scrollBarHeight}px;`"
-    @scroll="handleScroll"
-    ref="scrollbar"
-  >
-    <el-table
-      :data="tableData4"
-      :style="{
-        width: '100%',
-      }"
-      ref="tableRef"
-    >
-      <el-table-column
-        prop="date"
-        label="日期"
-        width="180"
-        :filters="[
-          { text: '2016-05-01', value: '2016-05-01' },
-          { text: '2016-05-02', value: '2016-05-02' },
-        ]"
-      >
-      </el-table-column>
-      <el-table-column prop="id" label="id" width="180"> </el-table-column>
-    </el-table>
-  </el-scrollbar>
-  
+  <table-form
+    v-model="tableData2"
+    :cols="cols"
+    @change="changeHandler"
+    :readonly="false"
+    class="case-mock"
+  ></table-form>
 </template>
 
 <script lang="ts">
 import {
-  computed,
   defineComponent,
   ref,
 } from 'vue';
-import { NormalObject, TableFormTable } from '@/components/TableForm/types';
-import { ElScrollbar } from 'element-plus';
-import { uniqueId } from 'lodash-es';
+import TableForm from '@/components/TableForm/index.vue';
 
+import {caseMock, priorityOptions, testResultsOptions} from './mock';
 export default defineComponent({
   props: {},
   components: {
-    ElScrollbar,
+    TableForm,
   },
-  setup(props, { emit }) {
-    const tableData3 = ref(
-      new Array(100).fill(0).map((k, i) => ({
-        date: i + 1,
-        id: uniqueId('fri_'),
-      }))
-    );
-    const realScrollTop = ref(0);
-    // const start = ref(0);
-    // const end = ref(0);
-    const size = ref(10);
-    const extra = 4;
-    const itemHeight = ref(48);
-    const scrollBarHeight = computed(
-      () => tableData3.value.length * itemHeight.value
-    );
+  setup() {
+    const cols = ref([
+      {
+        type: 'selection',
+        width: 55,
+        name: '',
+      },
+      {
+        type: 'index',
+        width: 60,
+        label: '序号',
+        align: 'center',
+      },
+      {
+        label: '功能描述',
+        name: 'function',
+        minWidth: '150px',
+        type: 'textarea',
+        rules: [
+          { required: true, message: '该项必填' },
+          // { validator: validateFunc, trigger: 'change' },
+        ],
+        attrs: { placeholder: '请输入', wrap: 'hard', resize: 'none' },
+        formProps: { rows: '2', wrap: 'hard' },
+      },
+      {
+        label: '前置条件',
+        name: 'precondition',
+        minWidth: '150px',
+        type: 'textarea',
+        rules: [{ required: true, message: '该项必填' }],
+        attrs: { placeholder: '请输入', wrap: 'hard', resize: 'none' },
+        formProps: {},
+      },
+      {
+        label: '步骤描述',
+        name: 'steps',
+        minWidth: '150px',
+        type: 'textarea',
+        rules: [{ required: true, message: '该项必填' }],
+        attrs: { placeholder: '请输入', wrap: 'hard', resize: 'none' },
+        formProps: { rows: '2', wrap: 'hard' },
+      },
+      {
+        label: '预期结果',
+        name: 'expectedResults',
+        minWidth: '150px',
+        type: 'textarea',
+        rules: [{ required: true, message: '该项必填' }],
+        attrs: { placeholder: '请输入', wrap: 'hard', resize: 'none' },
+        formProps: { type: 'textarea', rows: '2', wrap: 'hard' },
+      },
+      {
+        label: '优先级',
+        name: 'priority',
+        type: 'select',
+        filterInitialValue: [],
+        rules: [{ required: true, message: '该项必填' }],
+        attrs: { placeholder: '请输入' },
+        options: priorityOptions,
+      },
+      {
+        label: '测试结果',
+        name: 'testResults',
+        type: 'select',
+        filterInitialValue: [],
+        rules: [{ required: true, message: '该项必填' }],
+        attrs: { placeholder: '请输入' },
+        options: testResultsOptions,
+        // TODO: 左固定注意头部 有部分gutter空白样式要处理
+        // fixed: "left" 
+        // fixed: "right"
+      },
+    ]);
+    const tableData = ref([
+      {
+        isTested: '1',
+        functionList: ['1'],
+        percentage: 60,
+        testResults: [],
+        precondition: '前置条件',
+        priority: '0ed09e44-0fd0-4f8b-81d9-ef60139877a3',
+        // priority: '',
+        steps: '步骤描述',
+        function: '功能描述',
+        modifyTime: null,
+        id: '9a57b914-3fcb-4b40-878f-b6357787cafd',
+        sn: null,
+        expectedResults: '预期结果',
+        tranformSteps: null,
+        testCaseId: '38584329-3187-4475-a272-ff455d24b480',
+        // readonly: true,
+      },
+      {
+        isTested: '1',
+        functionList: ['1'],
+        percentage: 60,
+        testResults: [],
+        precondition: '前置条件',
+        priority: 'ac9739ce-b575-4f8a-b4c2-e4102f2b10d0',
+        steps: '步骤描述',
+        function: '功能描述',
+        modifyTime: null,
+        id: '9a57b914-3fcb-4b40-878f-b6357787cafd2',
+        sn: null,
+        expectedResults: '预期结果',
+        tranformSteps: null,
+        testCaseId: '38584329-3187-4475-a272-ff455d24b480',
+        readonly: true,
+      },
+    ]);
 
-    const scrollbarWrap = computed(() => {
-      return scrollbar.value?.wrap;
-    });
-    const tableRef = ref();
-    const handleScroll = (e: { scrollLeft: number; scrollTop: number }) => {
-      const { scrollTop } = e;
-      const top = (scrollTop * scrollbarWrap.value.clientHeight) / 100;
-      realScrollTop.value = top;
-
-      if (tableRef.value) {
-        const table = (tableRef.value as TableFormTable<NormalObject>);
-        const tableHeader = table.$refs.headerWrapper;
-        const tableBody = table.$refs.bodyWrapper;
-        
-        const tableHeaderHeight = tableHeader.clientHeight;
-        tableHeader.style.position = 'absolute';
-        tableHeader.style.top = '0';
-        if (top) {
-          // tableHeader.style.position = 'absolute';
-          tableHeader.style.zIndex = '100';
-          // // TODO: 注意这里逻辑 调试了很久
-          // tableHeader.style.top = `${top - (start.value * itemHeight.value)}px`;
-          
-          // (tableRef.value.$el as HTMLElement).style.paddingTop =
-          //   tableHeaderHeight + 'px';
-          tableBody.style.transform = `translateY(${start.value * itemHeight.value}px)`;
-        } else {
-          tableBody.style.position = 'static';
-          tableBody.style.transform = `translateY(0px)`;
-        }
-      }
+    const changeHandler = (e: any) => {
+      console.log(e);
     };
-    const start = computed(() => {
-      let begin = Math.ceil(realScrollTop.value / itemHeight.value) - extra;
-      begin = begin > 0 ? begin : 0;
-      return begin;
-    });
-    const end = computed(() => {
-      let total = tableData3.value.length;
-      let stop = start.value + size.value + extra;
-      stop = stop > total - 1 ? total - 1 : stop;
-      return stop;
-    });
 
-    const tableData4 = computed(() => {
-      return tableData3.value.slice(start.value, end.value);
-    });
-    const scrollbar = ref(null);
+    const tableData2 = ref(caseMock);
 
     return {
+      tableData,
+      tableData2,
+      cols,
+      changeHandler,
 
-      handleScroll,
-      tableData3,
-      tableData4,
-      realScrollTop,
-      start,
-      end,
-      scrollBarHeight,
-      scrollbar,
-      scrollbarWrap,
-      size,
-      itemHeight,
-      tableRef,
     };
   },
 });
 </script>
 <style lang="scss">
-.scrollbar__wrap {
-  max-height: 400px;
-}
-.el-table__body-wrapper {
-  // position: absolute;
-}
+.case-mock {
+  width: 1200px;
+  margin: 20px auto;
+} 
 </style>
